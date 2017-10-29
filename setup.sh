213b0c986f70203fa6d950992e4522a275e49871
Tom Jorquera <tom@jorquera.net>
2017-10-29 15:29:53 +0100

    add setup script to create all the required env files
    also prefix env variables with `CHATONS_` to avoid conflicts
    e.g. `MAIL` was conflicting with system env variable (and losing)

---
+++ setup.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Set variables
+export CHATONS_ROOT_DIR=/srv
+export CHATONS_DOMAIN=oxyta.net
+export CHATONS_MAIL=services@$CHATONS_DOMAIN
+export CHATONS_MYSQL_PASSWORD=$(openssl rand -base64 32)
+export CHATONS_MYSQL_ROOT_PASSWORD=$(openssl rand -base64 32)
+
+if [ ! -d "$CHATONS_ROOT_DIR" ]; then
+    echo "error: $CHATONS_ROOT_DIR must exist and be a regular folder" >&2
+    exit 1
+fi
+
+# Export common environment variables
+
+echo "CHATONS_ROOT_DIR=$CHATONS_ROOT_DIR" > .env
+echo "CHATONS_DOMAIN=$CHATONS_DOMAIN" >> .env
+echo "CHATONS_MAIL=$CHATONS_MAIL" >> .env
+
+# Export specific environment configuration
+
+echo "MYSQL_PASSWORD=$CHATONS_MYSQL_PASSWORD" > cloud/.env
+echo "MYSQL_ROOT_PASSWORD=$CHATONS_MYSQL_ROOT_PASSWORD" >> cloud/.env
+echo "MYSQL_ROOT_PASSWORD=$CHATONS_MYSQL_ROOT_PASSWORD" > pad/.env
+echo "ETHERPAD_DB_PASSWORD=$CHATONS_MYSQL_ROOT_PASSWORD" >> pad/.env
+
